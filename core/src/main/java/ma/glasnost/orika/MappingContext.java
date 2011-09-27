48316c6057e20c3f3a4e315b417d475b781ca1ae
elaatifi <elaatifi@gmail.com>
2011-09-27 20:54:43 +0000

    Add register a concrete class for one mapping operation
    git-svn-id: https://orika.googlecode.com/svn/trunk@208 f4a4e40d-1d15-de9b-c3e8-c5ea35f5a4c4

--- core/src/main/java/ma/glasnost/orika/MappingContext.java
+++ core/src/main/java/ma/glasnost/orika/MappingContext.java
@@ -22,34 +22,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MappingContext {
-
-	private final Map<Class<?>, Class<?>> mapping;
-	private final Map<Object, Object> cache;
-
-	public MappingContext() {
-		mapping = new HashMap<Class<?>, Class<?>>();
-		cache = new HashMap<Object, Object>();
-	}
-
-	@SuppressWarnings("unchecked")
-	public <S, D> Class<? extends D> getConcreteClass(Class<S> sourceClass, Class<D> destinationClass) {
-
-		Class<?> clazz = mapping.get(sourceClass);
-		if (clazz != null && destinationClass.isAssignableFrom(clazz)) {
-			return (Class<? extends D>) clazz;
-		}
-		return null;
-	}
-
-	public <S, D> void cacheMappedObject(S source, D destination) {
-		cache.put(source, destination);
-	}
-
-	public <S> boolean isAlreadyMapped(S source) {
-		return cache.containsKey(source);
-	}
-
-	public Object getMappedObject(Object source) {
-		return cache.get(source);
-	}
+    
+    private final Map<Class<?>, Class<?>> mapping;
+    private final Map<Object, Object> cache;
+    
+    public MappingContext() {
+        mapping = new HashMap<Class<?>, Class<?>>();
+        cache = new HashMap<Object, Object>();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public <S, D> Class<? extends D> getConcreteClass(Class<S> sourceClass, Class<D> destinationClass) {
+        
+        Class<?> clazz = mapping.get(sourceClass);
+        if (clazz != null && destinationClass.isAssignableFrom(clazz)) {
+            return (Class<? extends D>) clazz;
+        }
+        return null;
+    }
+    
+    public void registerConcreteClass(Class<?> subjectClass, Class<?> concreteClass) {
+        mapping.put(subjectClass, concreteClass);
+    }
+    
+    public <S, D> void cacheMappedObject(S source, D destination) {
+        cache.put(source, destination);
+    }
+    
+    public <S> boolean isAlreadyMapped(S source) {
+        return cache.containsKey(source);
+    }
+    
+    public Object getMappedObject(Object source) {
+        return cache.get(source);
+    }
 }
