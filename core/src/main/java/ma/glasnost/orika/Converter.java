b50eac683cc5168f3d5d4aad1ce45193b1d92cc9
matt-deboer <matt.deboer@gmail.com>
2012-04-12 23:20:22 +0000

    reverted premature Converter change
    git-svn-id: https://orika.googlecode.com/svn/trunk@351 f4a4e40d-1d15-de9b-c3e8-c5ea35f5a4c4

--- core/src/main/java/ma/glasnost/orika/Converter.java
+++ core/src/main/java/ma/glasnost/orika/Converter.java
@@ -19,42 +19,9 @@ package ma.glasnost.orika;
 
 import ma.glasnost.orika.metadata.Type;
 
-/**
- * Converter is used to provide direct conversion from one type to another,
- * useful for those scenarios where complete control over the mapping is
- * desired.<br><br>
- * 
- * Note that an instance of the current MapperFacade is provided in the <code>convert</code>
- * method for cases where you only want to control a specific portion of the mapping, 
- * but wish to delegate some or all of the mapping of the nested types.
- * <br><br>
- * 
- * See also {@link ma.glasnost.orika.CustomConverter} for a base class which can be
- * extended to create your own custom converter instance.
- *
- * @param <S>
- * @param <D>
- */
 public interface Converter<S, D> {
     
-    /**
-     * Answers whether this converter can be used to handle the conversion of <code>sourceType</code>
-     * to <code>destinationType</code>.
-     * 
-     * @param sourceType
-     * @param destinationType
-     * @return
-     */
     boolean canConvert(Type<?> sourceType, Type<?> destinationType);
     
-    /**
-     * Perform the conversion of <code>source</code> into a new instance of
-     * <code>destinationType</code>.
-     * 
-     * @param source the source object to be converted
-     * @param destinationType the destination type to produce
-     * @param mapper the current MapperFacade
-     * @return a new instance of <code>destinationType</code>
-     */
-    D convert(S source, Type<? extends D> destinationType, MapperFacade mapper);
+    D convert(S source, Type<? extends D> destinationType);
 }
