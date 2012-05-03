52d4167312b8f5b9e38b2bfabb47e83d8e573333
matt-deboer <matt.deboer@gmail.com>
2012-05-03 20:25:05 +0000

    Added license information and initial javadocs for recent commits.
    git-svn-id: https://orika.googlecode.com/svn/trunk@395 f4a4e40d-1d15-de9b-c3e8-c5ea35f5a4c4

--- core/src/main/java/ma/glasnost/orika/impl/mapping/strategy/MappingStrategy.java
+++ core/src/main/java/ma/glasnost/orika/impl/mapping/strategy/MappingStrategy.java
@@ -1,9 +1,43 @@
+/*
+ * Orika - simpler, better and faster Java bean mapping
+ * 
+ * Copyright (C) 2011 Orika authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ma.glasnost.orika.impl.mapping.strategy;
 
 import ma.glasnost.orika.MappingContext;
 
+/**
+ * MappingStrategy defines the contract for a pre-resolved classification of mapping
+ * which may be cached for quick lookup based on raw inputs.
+ * 
+ * @author matt.deboer@gmail.com
+ *
+ */
 public interface MappingStrategy {
     
+    /**
+     * Perform the mapping
+     * 
+     * @param sourceObject the source object to map
+     * @param destinationObject the pre-instantiated destination object onto which properties
+     * should be copied; may be null
+     * @param context the current mapping context
+     * @return the mapping result
+     */
     public Object map(Object sourceObject, Object destinationObject, MappingContext context);
  
 }
