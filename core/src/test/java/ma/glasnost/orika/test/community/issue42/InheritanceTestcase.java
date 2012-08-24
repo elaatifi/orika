f1292d6cd8c5e175de7b509e750e79a28d055fab
elaatifi <elaatifi@gmail.com>
2012-08-24 22:54:11 +0000

    Fix  Issue 42
    git-svn-id: https://orika.googlecode.com/svn/trunk@530 f4a4e40d-1d15-de9b-c3e8-c5ea35f5a4c4

--- core/src/test/java/ma/glasnost/orika/test/community/issue42/InheritanceTestcase.java
+++ core/src/test/java/ma/glasnost/orika/test/community/issue42/InheritanceTestcase.java
@@ -8,6 +8,7 @@ import ma.glasnost.orika.impl.DefaultMapperFactory;
 import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
 import ma.glasnost.orika.metadata.ClassMapBuilder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class InheritanceTestcase {
@@ -47,45 +48,13 @@ public class InheritanceTestcase {
         customer.setName("Customer Name");
         customer.setEmail("test@test.org");
         
-        /* Map from Customer to CustomerDto works fine: */
         CustomerDto customerDto = mapperFacade.map(customer, CustomerDto.class);
         
-        /*
-         * Map from CustomerDto to Customer throws ClassCastException: Exception
-         * in thread "main" ma.glasnost.orika.MappingException: Error
-         * encountered while mapping for the following inputs:
-         * rawSource=de.tib.ite
-         * .gwtpoc.mapping.InheritanceTestcase$CustomerDto@5ed364c
-         * sourceClass=class
-         * de.tib.ite.gwtpoc.mapping.InheritanceTestcase$CustomerDto
-         * sourceType=CustomerDto destinationType=Customer at
-         * ma.glasnost.orika.impl
-         * .MapperFacadeImpl.map(MapperFacadeImpl.java:278) at
-         * ma.glasnost.orika.
-         * impl.MapperFacadeImpl.map(MapperFacadeImpl.java:147) at
-         * ma.glasnost.orika
-         * .impl.MapperFacadeImpl.map(MapperFacadeImpl.java:622) at
-         * de.tib.ite.gwtpoc
-         * .mapping.InheritanceTestcase.testInheritance(InheritanceTestcase
-         * .java:50) at de.tib.ite.gwtpoc.Main.main(Main.java:56) Caused by:
-         * java.lang.ClassCastException:
-         * de.tib.ite.gwtpoc.mapping.InheritanceTestcase$Customer cannot be cast
-         * to de.tib.ite.gwtpoc.mapping. InheritanceTestcase$BaseUserDto at
-         * ma.glasnost
-         * .orika.generated.OrikaBaseUserDtoBaseUserMapper1254604688.mapBtoA
-         * (OrikaBaseUserDtoBaseUserMapper1254604688.java:25) at
-         * ma.glasnost.orika
-         * .impl.GeneratedMapperBase.mapBtoA(GeneratedMapperBase.java:105) at
-         * ma.
-         * glasnost.orika.generated.OrikaCustomerDtoCustomerMapper2022515739.
-         * mapBtoA (OrikaCustomerDtoCustomerMapper2022515739.java:23) at
-         * ma.glasnost.orika
-         * .impl.MapperFacadeImpl.mapDeclaredProperties(MapperFacadeImpl
-         * .java:533) at
-         * ma.glasnost.orika.impl.MapperFacadeImpl.map(MapperFacadeImpl
-         * .java:264) ... 4 more
-         */
         Customer c = mapperFacade.map(customerDto, Customer.class);
+        
+        Assert.assertEquals(customer.getEmail(), c.getEmail());
+        Assert.assertEquals(customer.getId(), c.getId());
+        
     }
     
     public static class BaseEntity {
