fe2fbf7ca20cfcd0b5c710e584b02f95a0778906
matt-deboer <matt.deboer@gmail.com>
2012-10-29 23:36:16 -0700

    cleaned up expander test case

--- core/src/test/java/ma/glasnost/orika/test/custommapper/ExpanderTestCase.java
+++ core/src/test/java/ma/glasnost/orika/test/custommapper/ExpanderTestCase.java
@@ -53,15 +53,9 @@ public class ExpanderTestCase {
         
         MapperFactory mapperFactory = new DefaultMapperFactory.Builder()
             .classMapBuilderFactory(new ScoringClassMapBuilder.Factory())
-            .compilerStrategy(new EclipseJdtCompilerStrategy())
             .build();
         
-        ClassMap<List<FlatData>, List<Year>> classMap = 
-                mapperFactory.classMap(typeOf_FlatData, typeOf_Year)
-                    .byDefault().toClassMap();
-        Assert.assertNotNull(classMap);
-        
-        mapperFactory.registerClassMap(classMap);
+        mapperFactory.classMap(typeOf_FlatData, typeOf_Year).byDefault().register();
         
         MapperFacade mapper = mapperFactory.getMapperFacade();
         
@@ -93,136 +87,5 @@ public class ExpanderTestCase {
         Year year = years.get(0);
         Assert.assertEquals(2011, year.yearNumber);
         Assert.assertEquals(2, year.months.size());
-    }
-    
-    /**
-     * Takes a list of flattened data elements and expands them into a hierarchical
-     * object graph who's root elements are of the target type provided.
-     * 
-     * @param from
-     * @param toType
-     * @return
-     */
-    private static void expand(List<FlatData> source, List<Year> destination) {
-        
-        MapperFacade mapperFacade = MappingUtil.getMapperFactory().getMapperFacade();
-        // goal is to expand a list of flattened data (such as you might receive
-        // from an SQL result set)
-        // into a hierarchical object graph, by auto-detecting where the nested
-        // values should be placed
-        
-        // 1. need to detect and match up all of the nested properties
-        // (including element properties)
-        // from the destination tpe
-        Iterator<FlatData> sourceIter = source.iterator();
-        List<Year> newDestination = new ArrayList<Year>();
-        
-        Year currentDestinationElement = null; // newObjectMap
-        Month currentMonth = null;
-        Day currentDay = null;
-        
-        while (sourceIter.hasNext()) {
-            FlatData sourceElement = sourceIter.next();
-            
-            if (currentDestinationElement == null || 
-                    // comparator for currentDestinationElement
-                    (currentDestinationElement.yearNumber != sourceElement.yearNumber 
-                        || !currentDestinationElement.yearAnimal.equals(sourceElement.yearAnimal))
-                    ) {
-                // Add currentDestinationElement to it's container
-                newDestination.add(currentDestinationElement);
-                currentDestinationElement = new Year();
-                
-            }
-            mapperFacade.map(sourceElement, currentDestinationElement);
-            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-            if (currentMonth == null || 
-                    // comparator for currentMonth
-                    (currentMonth.monthNumber != sourceElement.monthNumber)
-                    ) {
-                // Add currentMonth to it's container
-                if (currentMonth != null) {
-                    currentDestinationElement.months.add(currentMonth);
-                }
-                currentMonth = new Month();
-                
-            }
-            mapperFacade.map(sourceElement, currentMonth);
-            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-            if (currentDay == null || 
-                    // comparator for currentMonth
-                    (currentDay.dayNumber != sourceElement.dayNumber)
-                    ) {
-                // Add currentMonth to it's container
-                currentMonth.days.add(currentDay);
-                currentDay = new Day();
-                
-            }
-            mapperFacade.map(sourceElement, currentDay);
-
-        }
-        
-        
-        // Add currentDestinationElement to it's container
-        newDestination.add(currentDestinationElement);
-        // Add currentMonth to it's container
-        currentDestinationElement.months.add(currentMonth);
-        // Add currentMonth to it's container
-        currentMonth.days.add(currentDay);
-        
-        
-        // Add new destination to the target destination
-        destination.clear();
-        destination.addAll(newDestination);
-    }
-    
-    private static void contract( List<Year> source, List<FlatData> destination) {
-        
-        MapperFacade mapperFacade = MappingUtil.getMapperFactory().getMapperFacade();
-        // goal is to expand a list of flattened data (such as you might receive
-        // from an SQL result set)
-        // into a hierarchical object graph, by auto-detecting where the nested
-        // values should be placed
-        
-        // 1. need to detect and match up all of the nested properties
-        // (including element properties)
-        // from the destination tpe
-        Iterator<Year> sourceIter = source.iterator();
-        List<FlatData> newDestination = new ArrayList<FlatData>();
-        
-        FlatData currentDestinationElement = null; // newObjectMap
-        while (sourceIter.hasNext()) {
-            Year sourceElement = sourceIter.next();
-            
-            mapperFacade.map(sourceElement, currentDestinationElement);
-            
-            Iterator<Month> month_sourceIter = sourceElement.months.iterator();
-            while (month_sourceIter.hasNext()) {
-                
-                Month months_sourceElement = month_sourceIter.next();
-                mapperFacade.map(months_sourceElement, currentDestinationElement);
-                
-                Iterator<Day> days_sourceIter = months_sourceElement.days.iterator();
-                while( days_sourceIter.hasNext()) {
-                    
-                    Day days_sourceElement = days_sourceIter.next();
-                    if (currentDestinationElement == null || 
-                            // comparator for currentDestinationElement
-                            (currentDestinationElement.yearNumber != sourceElement.yearNumber)
-                            ) {
-                        currentDestinationElement = new FlatData();
-                        // Add currentDestinationElement to it's container
-                        newDestination.add(currentDestinationElement);
-                    }
-                    
-                    mapperFacade.map(days_sourceElement, currentDestinationElement);
-                    
-                }
-                
-                
-                
-            }
-        }        
-    
     }
 }
