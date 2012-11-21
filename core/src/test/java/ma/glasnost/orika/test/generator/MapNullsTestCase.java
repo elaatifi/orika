deeb035a44ec3a22902f5ca15409f82fcbf91cb6
mdeboer <matt.deboer@gmail.com>
2012-11-20 23:24:09 -0800

    Added unit tests for mapping nulls at classmap and field levels

--- core/src/test/java/ma/glasnost/orika/test/generator/MapNullsTestCase.java
+++ core/src/test/java/ma/glasnost/orika/test/generator/MapNullsTestCase.java
@@ -49,6 +49,16 @@ public class MapNullsTestCase {
         public Position enumValue;
     }
     
+    public static class Container2 {
+        public long longValue;
+        public String stringValue;
+        public List<String> listOfString;
+        public String[] arrayOfString;
+        public int[] arrayOfInt;
+        public Map<String, Object> map;
+        public Position enumValue;
+    }
+    
     @Test
     public void mapNulls_False() {
 
@@ -101,4 +111,94 @@ public class MapNullsTestCase {
         Assert.assertNull( b.enumValue );
     }
     
+    @Test
+    public void mapNulls_True_ClassLevel() {
+
+        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().mapNulls(false).build();
+        
+        mapperFactory.classMap(Container.class, Container2.class)
+            .destinationMappedOnNull(true).byDefault().register();
+        
+        Container a = new Container();
+        Container2 b = new Container2();
+
+        b.longValue = 1L;
+        b.stringValue = "TEST A";
+        b.arrayOfString = new String[]{"a", "b", "c"};
+        b.arrayOfInt = new int[] {1,2,3};
+        b.listOfString = Arrays.asList("l1","l2");
+        b.map = Collections.singletonMap("key", (Object)"value");
+        b.enumValue = Position.FIRST;
+        
+        mapperFactory.getMapperFacade().map(a, b);
+
+        Assert.assertNull( b.stringValue );
+        Assert.assertNull( b.arrayOfString );
+        Assert.assertNull( b.arrayOfInt );
+        Assert.assertNull( b.listOfString );
+        Assert.assertNull( b.map );
+        Assert.assertNull( b.enumValue );
+        
+        
+    }
+    
+    @Test
+    public void mapNulls_False_ClassLevel() {
+
+        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().mapNulls(true).build();
+        
+        mapperFactory.classMap(Container.class, Container2.class)
+        .destinationMappedOnNull(false).byDefault().register();
+    
+        Container a = new Container();
+        Container2 b = new Container2();
+
+        b.longValue = 1L;
+        b.stringValue = "TEST A";
+        b.arrayOfString = new String[]{"a", "b", "c"};
+        b.arrayOfInt = new int[] {1,2,3};
+        b.listOfString = Arrays.asList("l1","l2");
+        b.map = Collections.singletonMap("key", (Object)"value");
+        b.enumValue = Position.FIRST;
+        
+        mapperFactory.getMapperFacade().map(a, b);
+
+        Assert.assertNotNull( b.stringValue );
+        Assert.assertNotNull( b.arrayOfString );
+        Assert.assertNotNull( b.arrayOfInt );
+        Assert.assertNotNull( b.listOfString );
+        Assert.assertNotNull( b.map );
+        Assert.assertNotNull( b.enumValue );
+    }
+    
+    @Test
+    public void mapNulls_FieldLevel() {
+
+        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().mapNulls(false).build();
+        
+        mapperFactory.classMap(Container.class, Container2.class)
+            .destinationMappedOnNull(false)
+            .fieldMap("arrayOfString").destinationMappedOnNull(true).add()
+            .byDefault().register();
+    
+        Container a = new Container();
+        Container2 b = new Container2();
+
+        b.longValue = 1L;
+        b.stringValue = "TEST A";
+        b.arrayOfString = new String[]{"a", "b", "c"};
+        b.arrayOfInt = new int[] {1,2,3};
+        b.listOfString = Arrays.asList("l1","l2");
+        b.map = Collections.singletonMap("key", (Object)"value");
+        b.enumValue = Position.FIRST;
+        
+        mapperFactory.getMapperFacade().map(a, b);
+
+        Assert.assertNotNull( b.stringValue );
+        Assert.assertNull( b.arrayOfString );
+        Assert.assertNotNull( b.arrayOfInt );
+        Assert.assertNotNull( b.listOfString );
+        Assert.assertNotNull( b.map );
+        Assert.assertNotNull( b.enumValue );
+    }
 }
