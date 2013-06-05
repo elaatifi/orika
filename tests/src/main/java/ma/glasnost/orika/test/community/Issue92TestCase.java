db9ab64cfb5bf35d24c09e28835193b34fb38a99
matt-deboer <matt.deboer@gmail.com>
2013-06-05 02:14:55 -0700

    Cleaned up imports

--- tests/src/main/java/ma/glasnost/orika/test/community/Issue92TestCase.java
+++ tests/src/main/java/ma/glasnost/orika/test/community/Issue92TestCase.java
@@ -13,7 +13,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class Issue92TestCase {
