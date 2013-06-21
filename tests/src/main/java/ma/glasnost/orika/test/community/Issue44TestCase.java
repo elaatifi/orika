4f6fd10fe2cc8a9498f98290908f96728a890500
matt-deboer <matt.deboer@gmail.com>
2013-06-20 21:57:55 -0700

    Added test case for issue#44 which has already been resolved.

---
+++ tests/src/main/java/ma/glasnost/orika/test/community/Issue44TestCase.java
@@ -0,0 +1,127 @@
+package ma.glasnost.orika.test.community;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ma.glasnost.orika.CustomConverter;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+import ma.glasnost.orika.metadata.Type;
+
+import org.junit.Test;
+
+public class Issue44TestCase {
+    
+    @Test
+    public void shouldMapCollection() {
+        ConfigurableMapper mapper = new ConfigurableMapper() {
+            @Override
+            protected void configure(MapperFactory factory) {
+                factory.classMap(Order.class, OrderDto.class).byDefault().register();
+                factory.classMap(Product.class, ProductDto.class).byDefault().register();
+            }
+        };
+        Order order = new Order();
+        Product product = new Product();
+        product.setName("myName");
+        order.setProducts(asList(product));
+        OrderDto orderDto = mapper.map(order, OrderDto.class);
+        assertThat(orderDto.getProducts(), hasSize(1));
+        assertThat(orderDto.getProducts().get(0).getName(), is(equalTo("myName")));
+    }
+    
+    @Test
+    public void shouldMapCollectionWithConverter() {
+        ConfigurableMapper mapper = new ConfigurableMapper() {
+            @Override
+            protected void configure(MapperFactory factory) {
+                factory.getConverterFactory().registerConverter("productToName", new CustomConverter<List<Product>, List<String>>() {
+                    
+                    public List<String> convert(List<Product> source, Type<? extends List<String>> destinationType) {
+                        ArrayList<String> list = new ArrayList<String>(source.size());
+                        for (Product product : source) {
+                            list.add(product.getName());
+                        }
+                        return list;
+                    }
+                });
+                factory.classMap(Order.class, OrderDto.class)
+                        .fieldMap("products", "productNames")
+                        .converter("productToName")
+                        .add()
+                        .register();
+                factory.classMap(Product.class, ProductDto.class).byDefault().register();
+            }
+        };
+        Order order = new Order();
+        Product product = new Product();
+        product.setName("myName");
+        order.setProducts(asList(product));
+        OrderDto orderDto = mapper.map(order, OrderDto.class);
+        assertThat(orderDto.getProductNames(), hasSize(1));
+        assertThat(orderDto.getProductNames().get(0), is(equalTo("myName")));
+    }
+    
+    public static class Product {
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    
+    public static class Order {
+        private List<Product> products;
+        
+        public List<Product> getProducts() {
+            return products;
+        }
+        
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+    }
+    
+    public static class ProductDto {
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    
+    public static class OrderDto {
+        private List<ProductDto> products;
+        private List<String> productNames;
+        
+        public List<ProductDto> getProducts() {
+            return products;
+        }
+        
+        public void setProducts(List<ProductDto> products) {
+            this.products = products;
+        }
+        
+        public List<String> getProductNames() {
+            return productNames;
+        }
+        
+        public void setProductNames(List<String> productNames) {
+            this.productNames = productNames;
+        }
+    }
+}
