06eef93d42e6f0a8af964f7d24d7b0673a7d3240
Sidi Mohamed EL AATIFI <elaatifi@gmail.com>
2013-07-12 15:21:09 +0200

    Fix Array or collection to array mapping specification.
    When used for constructor mapping, a local variable get a duplicated name.

--- core/src/main/java/ma/glasnost/orika/impl/generator/specification/ArrayOrCollectionToArray.java
+++ core/src/main/java/ma/glasnost/orika/impl/generator/specification/ArrayOrCollectionToArray.java
@@ -21,20 +21,20 @@ public class ArrayOrCollectionToArray extends AbstractSpecification {
 
     public String generateMappingCode(FieldMap fieldMap, VariableRef source, VariableRef destination, SourceCodeContext code) {
         
-        final VariableRef arrayVar = destination.elementRef(destination.name());
-        String newArray = format("%s[] %s = new %s[%s]", destination.elementTypeName(), destination.name(), destination.elementTypeName(), source.size());
+        final VariableRef arrayVar = destination.elementRef(destination.name()+"Array__");
+        String newArray = format("%s[] %s = new %s[%s]", destination.elementTypeName(), arrayVar.name(), destination.elementTypeName(), source.size());
         
         String mapArray;
         if (destination.elementType().isPrimitive()) {
             if (code.isDebugEnabled()) {
                 code.debug("mapping to primitive array");
             }
-            mapArray = format("mapArray(%s, asList(%s), %s.class, mappingContext)", arrayVar, source, arrayVar.typeName());
+            mapArray = format("mapArray(%s, asList(%s), %s.class, mappingContext)", arrayVar.name(), source, arrayVar.typeName());
         } else {
             if (code.isDebugEnabled()) {
                 code.debug("mapping to array");
             }
-            mapArray = format("mapperFacade.mapAsArray(%s, asList(%s), %s, %s, mappingContext)", destination.name(), source, code.usedType(source.elementType()),
+            mapArray = format("mapperFacade.mapAsArray(%s, asList(%s), %s, %s, mappingContext)", arrayVar.name(), source, code.usedType(source.elementType()),
                     code.usedType(destination.elementType()));
         }
         String mapNull = shouldMapNulls(fieldMap, code) ? format(" else { %s; }", destination.assignIfPossible("null")) : "";
