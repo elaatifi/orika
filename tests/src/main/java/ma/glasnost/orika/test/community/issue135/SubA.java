ba8faf82a2fdc0fb4e3cb264ea22d40eb55c1676
Leonard Brünings <lord_damokles@gmx.net>
2013-11-27 10:34:43 +0100

    Added Testcase for Issue 135

---
+++ tests/src/main/java/ma/glasnost/orika/test/community/issue135/SubA.java
@@ -0,0 +1,15 @@
+package ma.glasnost.orika.test.community.issue135;
+
+public class SubA {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
